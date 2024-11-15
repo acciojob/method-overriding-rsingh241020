490c628d39a276aa77496bf2ec239828b5da9c75
Rohit Kumar <126482082+rsingh241020@users.noreply.github.com>
2024-11-15 22:34:29 +0530

    update the Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,28 +1,20 @@
 package com.driver;
 
-// Task 1: Create class A with a method named meth
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-// Task 2: Create class B which extends class A
-class B extends A {
-    // Task 4: Override method meth in class B
-    @Override
-    public String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
-
 public class Main {
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
-        // Task 3: Create object of class B and call method meth of class A with it
-        B b = new B();
-
-        // Call the overridden method
-        b.meth(); // Task 5: Output should show overridden method from class B
-        b.meth();
+        B obj = new B();
+        obj.meth();
+        obj.meth();
     }
 }
